59e4b1674a24828be7347fff7d9e2e945b184e6b
Milky Raven <heartonllamas@gmail.com>
2023-08-22 14:28:24 +0200

    set base for 3d landing page

---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function () {
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth"})
+  }
+  return (
+    <div className='display-section wrapper'>
+        <h2 className='title'>New</h2>
+        <p className='text'>Brilliant.</p>
+        <span className='description'>More text.</span>
+        <button className='button'>Try me!</button>
+        <button className='back-button' onClick={handleScrollToTop}>TOP</button>
+    </div>
+  )
+}
